732c2d8fd8cbf0e92b25670fc0fabe069a4dbf5d
gsdstr <gsdstr@gmail.com>
2024-01-18 14:45:56 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "media.licdn.com",
+        },
+      ],
+    },
+    experimental: {
+      serverActions: {
+        allowedOrigins: ['my-proxy.com', '*.my-proxy.com'],
+      },
+    },
+  };
 
-module.exports = nextConfig
+  module.exports = nextConfig;
