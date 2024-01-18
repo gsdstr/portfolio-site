fd4f17727beee5f806cfcf1a8f7ba2a6d03309d6
gsdstr <gsdstr@gmail.com>
2024-01-18 15:05:19 +0300

    try github pages

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "media.licdn.com",
-        },
-      ],
-    },
-    experimental: {
-      serverActions: {
-        allowedOrigins: ['my-proxy.com', '*.my-proxy.com'],
+  output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
       },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["my-proxy.com", "*.my-proxy.com"],
     },
-  };
+  },
+};
 
-  module.exports = nextConfig;
+module.exports = nextConfig;
