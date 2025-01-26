722aadf2b0da2e34096cff23a77e5c572a11bf32
Stepan Grigorovich <gsdstr@gmail.com>
2025-01-26 21:46:02 +0300

    Migrate to tailwind v4

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -45,8 +45,8 @@ export default function Experience() {
               }}
             >
               <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.company}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+              <p className="font-normal mt-0!">{item.company}</p>
+              <p className="mt-1! font-normal! text-gray-700 dark:text-white/75">
                 {item.location}
               </p>
             </VerticalTimelineElement>
